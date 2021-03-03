254fd1e248fab53c1fd0ae9bc03402fcae78fe25
sumba101 <sumanth.goku@gmail.com>
2021-03-03 20:03:34 +0530

    Made template code for Flask backend receiving url of twitter page to analyse

---
+++ flask-backend/receiver.py
@@ -0,0 +1,14 @@
+from flask import Flask 
+from flask_restful import Resource, Api
+app=Flask(__name__)
+api=Api(app)
+
+class Contro(Resource):
+	def get(self,url):
+		# Here url will have the twitter url, utilise this for the rest of the code
+		return {'about':'hello'+url}
+
+api.add_resource(Contro,'/<string:url>')
+
+if __name__=='__main__':
+	app.run(debug=True)
