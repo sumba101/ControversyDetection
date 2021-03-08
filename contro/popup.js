513783103793808854d1940fe4945a6c2b9520f2
sumba101 <sumanth.goku@gmail.com>
2021-03-08 11:44:26 +0530

    Scraper is done

--- contro/popup.js
+++ contro/popup.js
@@ -10,7 +10,23 @@ function executeDetection(){
         temp.innerHTML="<span class=\"spinner-border spinner-border-sm\"></span> \n  Loading...";
         chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
             let url = tabs[0].url;
-            console.log(url);
+
+			var res = url.split("/");
+			res.shift();
+			res.shift();
+			res.shift(); 
+			url=res.join('/');
+			
+            let link='http://localhost:5000/'+url;
+			
+            const response = fetch(link, {
+                method: 'GET',
+                mode:'no-cors',
+                headers: {
+                  'Content-Type': 'application/json'
+                }
+            });
+            console.log(response);
         });
 // Once code is done with an answer, the below code must be triggered for reset
         // temp.innerHTML="  <span></span>\n  Analyse";
