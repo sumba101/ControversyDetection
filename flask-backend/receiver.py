4282c624c14b80bef74d54188f8b4a42f6d6c235
sumba101 <sumanth.goku@gmail.com>
2021-03-30 12:01:45 +0530

    Changed backend code

--- flask-backend/receiver.py
+++ flask-backend/receiver.py
@@ -1,13 +1,13 @@
 from flask import Flask 
 from flask_restful import Resource, Api
+from scraper import driverFunction
 app=Flask(__name__)
 api=Api(app)
 
 class Contro(Resource):
 	def get(self,url):
 		# Here url will have the twitter url, utilise this for the rest of the code
-		print(url)
-		return {'result':'hello'+url}
+		return {'result':driverFunction(url)}
 
 api.add_resource(Contro,'/<string:url>')
 
--- flask-backend/scraper.py
+++ flask-backend/scraper.py
@@ -1,49 +1,10 @@
-from selenium import webdriver
+def driverFunction(url):
+# url : Url of the twitter page
+	
+	url= "https://twitter.com/" + url
+	output = processTweet(url)
+	output = dict(sorted(x.items(), key=lambda item: item[1]))
+	return output
 
-def helper(i,j):
-    val= driver.find_elements_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/section/div/div/div[{}]/div/div/article/div/div/div/div[2]/div[2]/div[2]/div[4]/div[{}]'.format(i,j))
-    if len(val)==0:
-        val= driver.find_elements_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/section/div/div/div[{}]/div/div/article/div/div/div/div[2]/div[2]/div[2]/div[3]/div[{}]'.format(i,j))
-
-    return val[0].text if len(val)!=0 else None
-
-class Tweets:
-    def __init__(self):
-        self.text=''
-        self.reply=''
-        self.retweet=''
-        self.like=''
-    def setText(self,tweettext):
-
-        self.text=tweettext
-
-    def setRRL(self,reply,retweet,like):
-        self.reply=reply
-        self.retweet=retweet
-        self.like=like
-
-def scraper(url):
-    link = 'https://twitter.com/'+url   
-    driver = webdriver.Chrome()
-
-    # link='https://twitter.com/BarackObama/status/1366878648544993281'
-
-    driver.get(link)
-
-    print(driver.title)
-
-    # probably needs a sleep here to allow for chrome to open up fully
-
-    allTweets=list()
-    no_of_tweets = 15
-    for i in range(1,no_of_tweets):
-        component = driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/section/div/div/div[{}]'.format(i))
-
-        if component.text != '':
-            tweet=Tweets()
-            tweet.setText(component.text)
-            reply= helper(i,1)
-            retweet= helper(i,2)
-            like= helper(i,3)
-            tweet.setRRL(reply,retweet,like)
-            allTweets.append(tweet)
+def processTweet(url):
+	return {"temp","temp"}
