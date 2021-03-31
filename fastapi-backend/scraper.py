9fa4d75917105ad4695e93029d9de42a4c95e142
mg1800 <monilgokani08@gmail.com>
2021-03-31 23:45:59 +0530

    added status scraper

--- fastapi-backend/scraper.py
+++ fastapi-backend/scraper.py
@@ -1,12 +1,121 @@
+import requests
+import os
+import json
+import pandas as pd
+import pickle
+import time
+BEARER = 'AAAAAAAAAAAAAAAAAAAAALo6NAEAAAAAP%2BhqPpvJq2FY5S2%2Bs28OwLyh7CE%3Dr1K8imd2QZvtIKFRICXmHUQsC1WcRBkDiVXExVJFjNZYpJBx8L'
+
+headers = {"Authorization": f'Bearer {BEARER}'}
+
+# main function to retrieve data from twitter using twitter API
+def connectToEndpoint(url, headers):
+    response = requests.request("GET", url, headers=headers)
+    # print(response.status_code)
+    if (response.status_code == 429):
+            print('Sleeping for 15 minutes')
+            time.sleep(15*61)
+            print('restarted')
+            response = requests.request("GET", url, headers=headers)
+            # print(response.headers['x-rate-limit-reset'])
+            # exit()
+    if response.status_code != 200:
+        raise Exception(
+            "Request returned an error: {} {}".format(
+                response.status_code, response.text
+            )
+        )
+    return response.json()
+
+# functions to create urls to use with connect_to_endpoint()
+def createTweetUrl(tid):
+    tweet_fields = "tweet.fields=author_id,conversation_id"
+    # Tweet fields are adjustable.
+    # Options include:
+    # attachments, author_id, context_annotations,
+    # conversation_id, created_at, entities, geo, id,
+    # in_reply_to_user_id, lang, non_public_metrics, organic_metrics,
+    # possibly_sensitive, promoted_metrics, public_metrics, referenced_tweets,
+    # source, text, and withheld
+    # You can adjust ids to include a single Tweets.
+    # Or you can add to up to 100 comma-separated IDs
+    url = f'https://api.twitter.com/2/tweets?ids={tid}&{tweet_fields}'
+    return url
+
+def createReplyUrl(tid, uid):
+	tweet_fields = "tweet.fields=author_id,conversation_id"
+	url = f'https://api.twitter.com/2/tweets/search/recent?query=(conversation_id:{tid} to:{uid})&max_results=50&{tweet_fields}'
+    # print(url)
+	return url
+
+# function to parse JSON response received from Twitter API
+def parseResponse(response):
+	data = []
+	try:
+		for tweet in response['data']:
+			tid = tweet['id']
+			text = tweet['text']
+			con_id = tweet['conversation_id']
+			auth_id = tweet['author_id']
+			info = tuple([tid, con_id, auth_id, text])
+			data.append(info)
+		return data
+	except KeyError:
+		return data
+
+# main driver code
 def driverFunction(url):
 # url : Url of the twitter page
 	
-	url= "https://twitter.com/" + url
-	output = processTweet(url)
-	output = dict(sorted(output.items(), key=lambda item: item[1]))
-	return output
+	# url= "https://twitter.com/" + url
+	# output = processTweet(url)
+	# output = dict(sorted(output.items(), key=lambda item: item[1]))
+	
+	string = url.split('/')
+	if len(string) == 1:
+		output = processProfile(url)
+	else:
+		print(string[-1])
+		root, replies = processStatus(string[-1])
+	
+	# print(root)
+	# print(replies.head())
+	return True
 
-def processTweet(url):
-	response = {"Every single American has a right to clean drinking water. It’s just plain wrong that in the United States of America today, millions of children still receive their water through lead service pipes. It’s long past time we fix that.":True}
+def processStatus(tid):
 
-	return response
+	# output = {"Every single American has a right to clean drinking water. It’s just plain wrong that in the United States of America today, millions of children still receive their water through lead service pipes. It’s long past time we fix that.":True}
+
+	tweetUrl = createTweetUrl(tid)
+	response = connectToEndpoint(tweetUrl, headers)
+
+	tweet = parseResponse(response)
+
+	if len(tweet) == 0:
+		# HANDLE THIS ERROR PLEASE IDK WHAT TO DO IF THIS HAPPENS
+		print('Tweet not found')
+		return tweet
+	
+	replyUrl = createReplyUrl(tweet[0][1], tweet[0][2])
+	response = connectToEndpoint(replyUrl, headers)
+
+	reply = parseResponse(response)
+
+	tweets = pd.DataFrame(tweet, columns = ['tweet_id', 'conversation_id', 'author_id', 'text'])
+
+	replies= pd.DataFrame(reply, columns = ['tweet_id', 'conversation_id', 'author_id', 'text'])
+
+
+	return tweets, replies
+
+def processProfile(name):
+
+
+
+	return True
+
+
+# if __name__ == '__main__':
+# 	url = 'elonmusk/status/1376901399867441156'
+
+# 	driverFunction(url)
