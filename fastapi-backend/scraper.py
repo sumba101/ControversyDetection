4bd4e0b403dfa6fac50911686106237ebffbfcf1
mg1800 <monilgokani08@gmail.com>
2021-04-17 16:38:13 +0530

    bug-fixes

--- fastapi-backend/scraper.py
+++ fastapi-backend/scraper.py
@@ -83,7 +83,7 @@ def fetch_score(text, comments):
     headers = CaseInsensitiveDict()
     headers["Content-Type"] = "application/json"
     res = requests.post("https://34d7e88b877b.ngrok.io/predict_status", headers=headers, data=json.dumps(text))
-    # print(res.json())
+    print(res)
     return res.json()['contro']
 
 # main driver code
@@ -108,8 +108,15 @@ def driverFunction(url):
 		# print(root)
 		text = root['text']
 		comments = []
-		reply = replies[replies['conversation_id'] == root['conversation_id']]
-		for j, tweet in reply.sample(10).iterrows():
+		con_id = root['conversation_id']
+		# print(con_id)
+		reply = replies[replies['conversation_id'] == con_id]
+		try:
+			sample_reply = reply.sample(10)
+		except ValueError:
+			sample_reply = reply 
+		# print(sample_reply)
+		for j, tweet in sample_reply.iterrows():
 			comments.append(tweet['text'])
 		results[text] = bool(fetch_score(text, comments))
 		
